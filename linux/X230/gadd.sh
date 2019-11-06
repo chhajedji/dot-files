fa02a0a0c184ce3f586c2b162e2cbb817b5997af
Chinmay <chhajedchinmay@gmail.com>
2019-11-06 21:55:51 +0530

    Files for X230

---
+++ linux/X230/gadd.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+cd ~/github/dot-files/
+cp -r ~/.config/i3 ~/.config/dunst ~/.config/compton ~/.config/gsimplecal ~/github/dot-files/linux/X230/.config/
+cp -r ~/.vimrc ~/gadd.sh ~/.profile ~/.bashrc ~/.alias ~/.fonts ~/github/dot-files/linux/X230/
+clear; git status
+echo "Copied
+~/
+    - .alias
+    - .vmirc
+    - gadd.sh
+    - .bashrc
+    - .profile
+    - .fonts/
+    - .config/
+        - i3/
+        - compton/
+        - gsimplecal/
+        - dunst/"
