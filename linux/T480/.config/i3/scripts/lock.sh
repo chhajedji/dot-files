4b8859b036863c813489c7ae9d5f1233e69fea0f
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2019-11-13 10:26:21 +0530

    Minor Changes

--- linux/T480/.config/i3/scripts/lock.sh
+++ linux/T480/.config/i3/scripts/lock.sh
@@ -6,4 +6,9 @@
 #    - `-n` option blocks until screen is unlocked.
 #    - After unlocking, set screen timeout to 300 secs
 
+<<<<<<< Updated upstream
 xset dpms 5; i3lock -ef --color 2f343f -n; xset dpms 600
+=======
+# xset dpms 5; i3lock -ef --color 2f343f -n; xset dpms 600
+xset dpms 8; pic_lock.sh -i $HOME/.config/i3/background.png; xset dpms 600
+>>>>>>> Stashed changes
