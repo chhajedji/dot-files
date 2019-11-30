9f7fe410c3edf0593b30eda1882550307c37df79
Chinmay <chhajedchinmay@gmail.com>
2019-11-30 12:23:17 +0530

    Minor Changes.

--- linux/X230/gadd.sh
+++ linux/X230/gadd.sh
@@ -2,6 +2,7 @@
 
 clear
 cd ~/github/dot-files/
+git checkout X230
 cp -r ~/.config/i3 ~/.config/compton ~/.config/gsimplecal ~/github/dot-files/linux/X230/.config/
 cp -r ~/.vimrc ~/gadd.sh ~/.profile ~/.bashrc ~/.alias ~/.fonts ~/github/dot-files/linux/X230/
 git status
