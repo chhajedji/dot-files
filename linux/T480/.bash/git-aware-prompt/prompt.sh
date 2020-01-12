35f2f021625562050f15a11c585dbcbab96ecf4c
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-01-12 18:16:45 +0530

    T480: bugfix - fixed bash command prompt showing git branch
    Signed-off-by: Chinmay Chhajed <chinmay.chhajed@espressif.com>

--- linux/T480/.bash/git-aware-prompt/prompt.sh
+++ linux/T480/.bash/git-aware-prompt/prompt.sh
@@ -15,7 +15,7 @@ find_git_branch() {
     # Based on: http://stackoverflow.com/a/13003854/170413
     local branch
     git_branch=$(git branch 2> /dev/null|grep '^*'|colrm 1 2)
-    if [ -z git_branch ]
+    if [ -z $git_branch ]
     then
         git_branch=$git_branch
     else
