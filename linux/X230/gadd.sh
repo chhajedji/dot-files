764db4f35d7736fce6dde138dd3995966f2d1697
Chinmay Chhajed <chhajedchinmay@gmail.com>
2020-01-18 18:43:45 +0530

    X230:   - git show and add combined.         - gitconfigured for global gitignore.
    Signed-off-by: Chinmay Chhajed <chhajedchinmay@gmail.com>

--- linux/X230/gadd.sh
+++ linux/X230/gadd.sh
@@ -3,5 +3,5 @@
 cd $HOME/github/dot-files/linux/X230
 git checkout X230
 cp -r $HOME/.config/i3 $HOME/.config/dunst $HOME/.config/compton $HOME/.config/gsimplecal $HOME/github/dot-files/linux/X230/.config/
-cp -r $HOME/.zshrc $HOME/.oh-my-zsh $HOME/.inputrc $HOME/.Xresources $HOME/.vimrc $HOME/gadd.sh $HOME/.profile $HOME/.bashrc $HOME/.alias $HOME/.fonts $HOME/github/dot-files/linux/X230/
+cp -r $HOME/.gitignore $HOME/.gitconfig $HOME/.zshrc $HOME/.oh-my-zsh $HOME/.inputrc $HOME/.Xresources $HOME/.vimrc $HOME/gadd.sh $HOME/.profile $HOME/.bashrc $HOME/.alias $HOME/.fonts $HOME/github/dot-files/linux/X230/
 git status
