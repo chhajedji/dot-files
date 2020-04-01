a65a210b22a4aeb89aee3163244b82339005354d
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-04-01 21:42:11 +0530

    T480: dmenu_input will show suggestions for man pages and dictionary.
    Signed-off-by: Chinmay Chhajed <chinmay.chhajed@espressif.com>

--- linux/T480/.config/i3/scripts/dmenu_input.sh
+++ linux/T480/.config/i3/scripts/dmenu_input.sh
@@ -4,9 +4,9 @@ case $1 in
 
 # Search dictionary.
     -d)
-        WORD=$(echo '' | dmenu -p "Find meaning of:" -fn "Inconsolata 12")
+        WORD=$(cat /usr/share/dict/words | dmenu -i -l 20 -p "Find meaning of:" -fn "Inconsolata 12")
         if [ -n "$WORD" ]; then
-            urxvt -name dropdown_dictionary -e sh -c "dict ${WORD} |less" 2> /dev/null
+            urxvt -name dropdown_dictionary -e sh -c "dict ${WORD} |less"
         fi
         ;;
 
@@ -15,23 +15,26 @@ case $1 in
         URL='https://www.google.com/search?q='
         QUERY=$(echo '' | dmenu -p "Google Search for:" -fn "-xos4-terminus-medium-r-*-*-14-*")
         if [ -n "$QUERY" ]; then
-            xdg-open "${URL}${QUERY}" 2> /dev/null
-            exec i3-msg [class="^Firefox$"] focus 2>/dev/null 1>&2
+            xdg-open "${URL}${QUERY}"
+            exec i3-msg [class="^Firefox$"] focus >/dev/null
         fi
         ;;
 
 # Manual page.
     -m)
-        WORD=$(echo '' | dmenu -p "Manual page for:" -fn "Inconsolata 12")
+        # Store list of all available man pages in a file.
+        apropos . |sort >$HOME/.config/i3/other_files/manlist.txt
+        WORD=$(cat $HOME/.config/i3/other_files/manlist.txt | dmenu -l 20 -p "Manual page for:" -fn "Inconsolata 12" | cut -d ' ' -f 1)
+        echo $WORD
         if [ -n "$WORD" ]; then
-            urxvt -name dropdown_manual -e sh -c "man ${WORD} || figlet -c 'No manual entry for \"${WORD}\"' |less" 2> /dev/null
-            # exec i3-msg [instance="dropdown_manual"] focus 2>/dev/null 1>&2
+            urxvt -name dropdown_manual -e sh -c "man ${WORD} || figlet -c 'No manual entry for \"${WORD}\"' |less" >/dev/null
+            # exec i3-msg [instance="dropdown_manual"] focus >/dev/null
         fi
         ;;
 
     *)
         MESSAGE="Not a valid option to run with dmenu. :P"
-            urxvt -name dropdown_default -e sh -c "figlet -c ${MESSAGE} |less" 2> /dev/null
+        urxvt -name dropdown_default -e sh -c "figlet -c ${MESSAGE} |less" >/dev/null
         ;;
 
 esac
