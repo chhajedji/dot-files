4ded6827c0225f567f465e38df44caba8ffc359b
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-04-02 10:50:15 +0530

    T480: Minor changes
        * bash_profile
            * Added it *back*.
    
        * gadd
            * Reorganised to look better and added bash_profile.
    
        * prompt.sh
            * Removed unwanted lines.
    
        * dunstrc
            * Color settings for different level of notifications.
    
    Signed-off-by: Chinmay Chhajed <chinmay.chhajed@espressif.com>

--- T480/.bash/prompt.sh
+++ T480/.bash/prompt.sh
@@ -1,15 +1,4 @@
-# find_git_branch() {
-    # # Based on: http://stackoverflow.com/a/13003854/170413
-    # local branch
-    # if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-        # if [[ "$branch" == "HEAD" ]]; then
-            # branch='detached*'
-        # fi
-        # git_branch="($branch)"
-    # else
-        # git_branch=""
-    # fi
-# }
+#!/bin/sh
 
 find_git_branch() {
     # Based on: http://stackoverflow.com/a/13003854/170413
@@ -23,28 +12,8 @@ find_git_branch() {
     fi
 }
 
-#    find_git_dirty() {
-#      local status=$(git status --porcelain 2> /dev/null)
-#      if [[ "$status" != "" ]]; then
-#        git_dirty='*'
-#      else
-#        git_dirty=''
-#      fi
-#    }
-
-#PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
-
 if [ -n "$PROMPT_COMMAND" ]; then
     PROMPT_COMMAND="find_git_branch;$PROMPT_COMMAND"
 else
     PROMPT_COMMAND="find_git_branch"
 fi
-
-# Default Git enabled prompt with dirty state
-# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
-
-# Another variant:
-# export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
-
-# Default Git enabled root prompt (for use with "sudo -s")
-# export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
