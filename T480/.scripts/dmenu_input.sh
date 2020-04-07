ecabae8b3a9190bdcb01446ed992049e96f05b87
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-04-07 12:15:07 +0530

    T480: Minor changes.
        * scripts/
            * Removed float-runner.sh.
    
        * .profile/
            * Removed $HOME from $CDPATH as having directory with a
              directory in current path with same name as a directory at $HOME
              gives priority to $HOME's directory first.
            * Set BROWSER environment variable.
    
        * dmenu_input
            * Global setting for fonts.
    
    Signed-off-by: Chinmay Chhajed <chinmay.chhajed@espressif.com>

--- T480/.scripts/dmenu_input.sh
+++ T480/.scripts/dmenu_input.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+DMENU_FONT1="Inconsolata 12"
+
 case $1 in
 
 # Search dictionary.
     -d)
-        WORD=$(cat /usr/share/dict/words | dmenu -i -l 20 -p "Find meaning of:" -fn "Inconsolata 12")
+        WORD=$(cat /usr/share/dict/words | dmenu -i -l 20 -p "Find meaning of:" -fn "$DMENU_FONT1")
         if [ -n "$WORD" ]; then
             urxvt -name dropdown_dictionary -e sh -c "dict ${WORD} |less"
         fi
@@ -14,7 +16,7 @@ case $1 in
     -g)
         SEARCHURL='https://www.google.com/search?q='
         GOTOURL='https://'
-        QUERY=$(echo '' | dmenu -p "Google Search for:" -fn "-xos4-terminus-medium-r-*-*-14-*")
+        QUERY=$(echo '' | dmenu -p "Search / Go to:" -fn "-xos4-terminus-medium-r-*-*-14-*")
         if [ -n "$QUERY" ]; then
             (echo $QUERY | grep ' ' >/dev/null && xdg-open "${SEARCHURL}${QUERY}" && echo case 1) ||
                 (echo $QUERY | grep '\.' >/dev/null && xdg-open "${GOTOURL}${QUERY}" && echo case 2) ||
@@ -27,7 +29,7 @@ case $1 in
     -m)
         # Store list of all available man pages in a file.
         apropos . |sort >$HOME/.config/i3/other_files/manlist.txt
-        WORD=$(cat $HOME/.config/i3/other_files/manlist.txt | dmenu -l 20 -p "Manual page for:" -fn "Inconsolata 12" | cut -d ' ' -f 1)
+        WORD=$(cat $HOME/.config/i3/other_files/manlist.txt | dmenu -l 20 -p "Manual page for:" -fn "$DMENU_FONT1" | cut -d ' ' -f 1)
         echo $WORD
         if [ -n "$WORD" ]; then
             urxvt -name dropdown_manual -e sh -c "man ${WORD} || figlet -c 'No manual entry for \"${WORD}\"' |less" >/dev/null
